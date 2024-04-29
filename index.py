9b78d87c411c713c8f9225f40372d3a57ed2b7f4
agrendath <mendo.leal@gmail.com>
2024-04-29 11:35:07 +0200

    new method of getting composition

--- index.py
+++ index.py
@@ -2,6 +2,7 @@ from __main__ import app
 import requests
 from datetime import datetime
 import overpy
+import composition
 
 overpass_api = overpy.Overpass()
 
@@ -18,18 +19,15 @@ def getLiveboard(station):
         return None
     return response
 
-def getComposition(train_id):
-    url = "http://api.irail.be/composition"
-    params = {"id": train_id, "format": "json"}
-    response = requests.get(url, params=params)
-    response = response.json()
-    if response is None:
-        print("[ERROR] Something went wrong while getting the train's composition")
-        return None
-    if "error" in response:
-        print("[ERROR] Something went wrong while getting the train's composition:", response["message"])
-        return None
-    return response
+def getComposition(station, departure): 
+    #print(composition.get_train_composition)
+    liveboard = getLiveboard(station)
+    from_id = liveboard["stationinfo"]["id"].split(".")[-1]
+    to_id = departure["stationinfo"]["id"].split(".")[-1]
+    vehicle = departure["vehicle"].split(".")[-1]
+    time = datetime.fromtimestamp(int(departure["time"]))
+    print("[DEBUG] Getting composition with " + from_id + " -> " + to_id + "; vehicle:" + vehicle + "; time:" + str(time))
+    print(composition.get_train_composition(from_id, to_id, vehicle, time))
 
 """
 Sample departure: {'id': '28', 'delay': '0', 'station': 'Kortrijk', 'stationinfo': {'locationX': '3.264549', 'locationY': '50.824506', 'id': 'BE.NMBS.008896008', 'name': 'Kortrijk', '@id': 'http://irail.be/stations/NMBS/008896008', 'standardname': 'Kortrijk'}, 'time': '1711892340', 'vehicle': 'BE.NMBS.IC2338', 'vehicleinfo': {'name': 'BE.NMBS.IC2338', 'shortname': 'IC 2338', 'number': '2338', 'type': 'IC', 'locationX': '0', 'locationY': '0', '@id': 'http://irail.be/vehicle/IC2338'}, 'platform': '13', 'platforminfo': {'name': '13', 'normal': '1'}, 'canceled': '0', 'left': '0', 'isExtra': '0', 'departureConnection': 'http://irail.be/connections/8814001/20240331/IC 2338'}
@@ -81,10 +79,9 @@ def getStandstillPosition(station, overpass_station_name, platform):
         return None  # No standstill position found
     train_id = departure["vehicle"].split(".")[-1]
     print(departure['vehicle'])
-    composition = getComposition(train_id)
+    composition = getComposition(station, departure)
     if composition is None:
         return None
-    #print(composition)
     # TODO: now that we have the composition, get static data and derive standstill position in function of train composition (# carriages)
     positions = getStandstillPositions(overpass_station_name).nodes
 
@@ -93,7 +90,7 @@ def getStandstillPosition(station, overpass_station_name, platform):
 def index():
     #print(getLiveboard("Brussels South"))
     #print(getComposition("S51507"))
-    #print(getNextDeparture("Brussels South", 13))
+    #print(getNextDeparture("Brussels North", 7))
     #print(getStandstillPosition("Brussels North", 7))
     print(getStandstillPosition("Brussels North", "Bruxelles-Nord - Brussel-Noord", 7))
     #print(getNextTrainInfo("Brussels North", 7))
