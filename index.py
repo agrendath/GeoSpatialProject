f830f829d3222074b78d87949c9a52f78fea6855
WhiteDarknessSG4 <leandrevilain@hotmail.com>
2024-05-08 18:27:47 +0200

    Ajout Icones + classes

--- index.py
+++ index.py
@@ -8,6 +8,7 @@ import composition
 
 overpass_api = overpy.Overpass()
 
+
 def getLiveboard(station):
     url = "http://api.irail.be/liveboard"
     params = {"station": station, "format": "json"}
@@ -21,13 +22,15 @@ def getLiveboard(station):
         return None
     return response
 
-def getComposition(station, departure): 
+
+def getComposition(station, departure):
     liveboard = getLiveboard(station)
     from_id = liveboard["stationinfo"]["id"].split(".")[-1]
     to_id = departure["stationinfo"]["id"].split(".")[-1]
     vehicle = departure["vehicleinfo"]["number"]
     time = datetime.fromtimestamp(int(departure["time"]))
-    print("[DEBUG] Getting composition with " + from_id + " -> " + to_id + "; vehicle:" + vehicle + "; time:" + str(time))
+    print(
+        "[DEBUG] Getting composition with " + from_id + " -> " + to_id + "; vehicle:" + vehicle + "; time:" + str(time))
     comp = composition.get_train_composition(from_id, to_id, vehicle, time)
     print(comp)
     composition_data = {
@@ -47,19 +50,23 @@ def getComposition(station, departure):
     }
     return composition_data
 
+
 """
 Sample departure: {'id': '28', 'delay': '0', 'station': 'Kortrijk', 'stationinfo': {'locationX': '3.264549', 'locationY': '50.824506', 'id': 'BE.NMBS.008896008', 'name': 'Kortrijk', '@id': 'http://irail.be/stations/NMBS/008896008', 'standardname': 'Kortrijk'}, 'time': '1711892340', 'vehicle': 'BE.NMBS.IC2338', 'vehicleinfo': {'name': 'BE.NMBS.IC2338', 'shortname': 'IC 2338', 'number': '2338', 'type': 'IC', 'locationX': '0', 'locationY': '0', '@id': 'http://irail.be/vehicle/IC2338'}, 'platform': '13', 'platforminfo': {'name': '13', 'normal': '1'}, 'canceled': '0', 'left': '0', 'isExtra': '0', 'departureConnection': 'http://irail.be/connections/8814001/20240331/IC 2338'}
 """
+
+
 def getNextDeparture(station, platform):
     liveboard = getLiveboard(station)
     if liveboard is None:
         return None
     departures = liveboard["departures"]["departure"]
     for departure in departures:
-        if int(departure["platform"]) == platform:
+        if departure["platform"] != "?" and int(departure["platform"]) == platform:
             return departure
     print("[ERROR] NO DEPARTURE FOUND")
-    return None # No departure found
+    return None  # No departure found
+
 
 """
 Returns some standard information about the next train departing from the given station and platform.
@@ -68,13 +75,15 @@ Returns some standard information about the next train departing from the given 
 :param platform: The platform where the train departs
 :return: A list with the destination, the vehicle name and the departure time in a readable format
 """
+
+
 def getNextTrainInfo(station, platform):
     departure = getNextDeparture(station, platform)
     destination = departure["station"]
     vehicle_name = departure["vehicleinfo"]["shortname"]
     time = datetime.fromtimestamp(int(departure["time"]))
     readable_time = time.strftime("%H:%M")
-    
+
     return [destination, vehicle_name, readable_time]
 
 
@@ -92,65 +101,167 @@ def getStandstillPositions(overpass_station_name):
     positions = []
     for node in response.nodes:
         position = {
+            "track": node.tags.get("ref:track", ""),
             "ref": node.tags.get("ref", ""),
             "lat": node.lat,
             "lon": node.lon
         }
         positions.append(position)
+
+    if (positions == []):
+        print("[ERROR] No signal points found.")
     return positions
+
+
+def getNodesWithTrack(nodes, track):
+    result = []
+    for node in nodes:
+        if node["track"] == str(track):
+            result.append(node)
+    if len(result) == 0:
+        print("[ERROR] Got empty list of nodes when looking for standstill positions for track " + str(track))
+    return result
+
+
+def getNodeWithRef(nodes, ref):
+    for node in nodes:
+        if node["ref"] == str(ref):
+            return node
+    print("[WARNING] No node found with ref " + str(ref))
+    return None
+
+
+def getZoneMarkers(overpass_station_name):
+    query = """
+    [out:json][timeout:25];
+    area(id:3600052411)->.searchArea;
+    (
+    nwr["railway"="station"][name=\"""" + overpass_station_name + """\"]["station"!="subway"]["tram"!="yes"](area.searchArea);
+    );
+    nwr["railway"="platform_marker"](around: 100.00);
+    out geom;
+    """
+
+    response = overpass_api.query(query)
+    zone_markers = []
+    for node in response.nodes:
+        marker = {
+            "track": node.tags.get("ref:track", ""),
+            "ref": node.tags.get("ref", ""),
+            "lat": node.lat,
+            "lon": node.lon
+        }
+        zone_markers.append(marker)
+
+    if (zone_markers == []):
+        print("[ERROR] No zone markers found.")
+
+    return zone_markers
+
+
+def getNextNextDeparture(station, platform):
+    liveboard = getLiveboard(station)
+    if liveboard is None:
+        return None
+    departures = liveboard["departures"]["departure"]
+    next_train_found = False
+    for departure in departures:
+        if next_train_found and departure["platform"] != "?" and int(departure["platform"]) == platform:
+            return departure
+        if departure["platform"] != "?" and int(departure["platform"]) == platform:
+            next_train_found = True
+    print("[ERROR] NO NEXT DEPARTURE FOUND")
+    return None  # No departure found
+
 
 def getStandstillPosition(station, overpass_station_name, platform):
     departure = getNextDeparture(station, platform)
-    print(departure)
     if departure is None:
         return None  # No standstill position found
+
+    next_departure = getNextNextDeparture(station, platform)
+    if next_departure is None:
+        return None  # No next departure found
+
     train_id = departure["vehicle"].split(".")[-1]
-    print(departure['vehicle'])
+    next_train_id = next_departure["vehicle"].split(".")[-1]
+
     composition_data = getComposition(station, departure)
     if composition_data is None:
         return None
 
+    next_composition_data = getComposition(station, next_departure)
+    if next_composition_data is None:
+        return None
+
     position_data = getStandstillPositions(overpass_station_name)
 
-    # Find the standstill position based on the number of carriages
-    carriages_count = composition_data["carriages_count"]
-    if carriages_count <= len(position_data):
-        standstill_position = position_data[carriages_count - 1]
-    else:
-        standstill_position = None
+    signals = getNodesWithTrack(position_data, platform)
+    carriages_amount = int(composition_data["carriages_count"])
+    temp_amount = carriages_amount
+    if carriages_amount % 2 == 1:
+        temp_amount -= 1
+    standstill_position = getNodeWithRef(signals, str(temp_amount))
+    if standstill_position is None:
+        print("[WARNING] Was not able to find standstill position for " + str(carriages_amount) + " carriages.")
 
-    # Create a clean output dictionary
+    all_zone_markers = getZoneMarkers(overpass_station_name)
+    track_zone_markers = getNodesWithTrack(all_zone_markers, platform)
+    if len(track_zone_markers) == 0:
+        print("[WARNING] No zone markers found for track " + str(platform))
+
     output = {
         "station": departure["station"],
         "destination": departure["stationinfo"]["name"],
         "vehicle_name": departure["vehicleinfo"]["shortname"],
         "departure_time": datetime.fromtimestamp(int(departure["time"])).strftime("%H:%M"),
         "composition": composition_data,
-        "standstill_position": standstill_position
+        "next_destination": next_departure["stationinfo"]["name"],
+        "next_vehicle_name": next_departure["vehicleinfo"]["shortname"],
+        "next_departure_time": datetime.fromtimestamp(int(next_departure["time"])).strftime("%H:%M"),
+        "next_composition": next_composition_data,
+        "standstill_position": standstill_position,
+        "zone_markers": track_zone_markers
     }
 
     return output
 
+
 @app.route("/", methods=['GET'])
 def index():
     station = "Brussels North"
-    platform = 8
-    standstill_position = getStandstillPosition(station, "Bruxelles-Nord - Brussel-Noord", platform)
+    station_overpass_name = "Bruxelles-Nord - Brussel-Noord"
+    platform = 1
+    standstill_position = getStandstillPosition(station, station_overpass_name, platform)
+
+    # return(f"{standstill_position}")
+    # standstill_position = None   #test with no standstill_position if standstill_position
+    #standstill_position = {'station': 'Nivelles', 'destination': 'Nivelles', 'vehicle_name': 'S1 1989','departure_time': '19:10','composition': {'facilities': ['airconditioning', 'heating'], 'occupancy': 'low', 'carriages_count': 10, 'carriages': [ {'carriage_type': 'left-wagon', 'model': 'AM08P_c', 'classes': [1, 2],'facilities': ['accessible_toilet', 'toilet', 'bike'], 'carriage_size': 18.4}, {'carriage_type': '', 'model': 'AM08P_b', 'classes': [2], 'facilities': [], 'carriage_size': 18.4},{'carriage_type': 'middle-wagon', 'model': 'AM08P_a', 'classes': [1, 2], 'facilities': [], 'carriage_size': 18.4}]},'standstill_position': None}  # test with standstill_position if no standstill_position
 
     if standstill_position is None:
-        return "No standstill position found."
+        return render_template('index.html', platform=f"{platform}",
+                               error=f"No standstill position found for {station}.")
 
     destination = standstill_position["destination"]
     vehicle_name = standstill_position["vehicle_name"]
     departure_time = standstill_position["departure_time"]
     composition_data = standstill_position["composition"]
     standstill_position_data = standstill_position["standstill_position"]
+    try:
+        next_destination = standstill_position["next_destination"]
+        next_departure_time = standstill_position["next_departure_time"]
+    except KeyError:
+        next_destination = "None"
+        next_departure_time = "None"
 
     carriages_info = []
+    i = 0
     for carriage in composition_data["carriages"]:
         carriage_type = carriage["carriage_type"]
+        carriage_classes = carriage["classes"]
         model = carriage["model"]
         facilities = carriage["facilities"]
+        carriages_icon = ""
         carriage_info = model
         if "bike" in facilities:
             carriage_info += " (Bike)"
@@ -159,14 +270,26 @@ def index():
         elif "toilet" in facilities:
             carriage_info += " (Toilet)"
 
+        carriage_info += f" - {carriage_classes}"
         carriages_info.append(carriage_info)
 
     carriages = ", ".join(carriages_info)
+
+    facilities = composition_data["facilities"]
 
     if standstill_position_data:
         position_info = f"Position: Ref - {standstill_position_data['ref']}, Lat - {standstill_position_data['lat']}, Lon - {standstill_position_data['lon']}"
     else:
         position_info = "No standstill position data found."
 
-    #return(f"{destination}" f"{vehicle_name}" f"{departure_time}" f"{composition_data}")
-    return render_template('index.html', destination = f"{destination}", vehicle_name = f"{vehicle_name}",departure_time = f"{departure_time}",facilities = f"{facilities}",composition_occupancy = f"{composition_data['occupancy']}",composition_carriages = f"{composition_data['carriages_count']}",carriages = f"{carriages}",position_info = f"{position_info}")
+    try:
+        zone_markers = standstill_position["zone_markers"]
+    except KeyError:
+        zone_markers = "None"
+
+    return render_template('index.html', error="No", platform=f"{platform}", destination=f"{destination}",
+                           vehicle_name=f"{vehicle_name}", departure_time=f"{departure_time}",
+                           facilities=f"{facilities}", composition_occupancy=f"{composition_data['occupancy']}",
+                           composition_carriages=f"{composition_data['carriages_count']}", carriages=f"{carriages}",
+                           position_info=f"{position_info}", zone_markers=f"{zone_markers}",
+                           next_destination=f"{next_destination}", next_departure_time=f"{next_departure_time}")
--- static/script/script.js
+++ static/script/script.js
@@ -1,1 +1,90 @@
-//alert('e')
+var container = document.createElement('div');
+container.setAttribute('id', 'container'); 
+var train = document.createElement('div');
+train.setAttribute('id', 'train'); 
+var zones = document.createElement('div');
+zones.setAttribute('id', 'zones'); 
+
+container.appendChild(zones)
+container.appendChild(train)
+
+//affichage des zones
+zones_names = "A,B,C,D,E"
+zones_names = zones_names.split(",")
+
+zones_names.forEach((zone)=>{
+    div = document.createElement("div")
+    div.setAttribute('class', 'zone')
+    div.textContent = zone
+    zones.appendChild(div)
+})
+
+var variablesElement = document.getElementById('variables');
+
+//var carriages = variablesElement.dataset.flaskVariable;//.split(",");
+
+var carriages2 = variablesElement.dataset.flaskVariable;
+
+var expression = /,(?![^\[\]]*])/;
+
+var carriages = carriages2.split(expression);
+
+console.log(carriages)
+
+nb_carriages = carriages.length
+//var nb_carriages = composition_carriages
+console.log(nb_carriages)
+
+/*displayImages(div,carriage){
+    if (condition) {
+        
+    }
+}*/
+
+carriages.forEach((carriage)=>{
+    div = document.createElement("div")
+    div.setAttribute('class', 'cb')
+    div.setAttribute('style', 'width='+100/nb_carriages+"%")
+
+    console.log(carriage)
+    
+    //Icons
+    if (carriage.includes("(Accessible toilet)")) {
+        div.innerHTML = "<div class='left-bottom'><i class='fa fa-wheelchair'></i></div>";
+    }
+
+    if (carriage.includes("(Toilet)")) {
+        div.innerHTML += "<div class='left-top'><i class='fa fa-restroom'></i></div>";
+    }
+    
+    if (carriage.includes("(Bike)")) {
+        div.innerHTML += "<div class='right-top'><i class='fa fa-bicycle'></i></div>";
+    }
+    
+   //Classes
+   if (carriage.includes("[1]")) {
+        div.innerHTML += "<div class='yellow'>1</div>";
+    }else if (carriage.includes("[2]")) {
+        div.innerHTML += "<div>2</div>";
+    }else if (carriage.includes("[1, 2]")) {
+        div.innerHTML += "<div><span class='yellow'>1</span>2</div>";
+    }
+    
+    train.appendChild(div)
+})
+
+
+tchou = document.getElementById('tchoutchou');
+tchou.appendChild(container)
+
+/*
+            <i class="fa fa-bicycle"></i><br>
+            <i class="fa fa-wheelchair"></i><br>
+            <i class="fa fa-arrow-right"></i>
+            <i class="fa fa-arrow-left"></i><br>
+            <i class="fa fa-arrow-circle-right"></i>
+            <i class="fa fa-arrow-circle-left"></i><br>
+            <i class="fa fa-volume-xmark"></i><br>
+            <i class="fa fa-stairs"></i><br>
+            <i class="fa fa-restroom"></i>
+*/
